731522ce04740e0f7a3340e81ceba4a837bed1c6
Tyler Schrock <tschrock123@gmail.com>
2019-09-07 13:11:27 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,15 @@
+@oauth-everything/oauth2-types
+=========================
+
+This package provides helper types for @oauth-everything OAuth 2.0 strategies.
+It has no actual code and should only be added as a dev dependency.
+
+## Instalation
+```bash
+npm install --save-dev @oauth-everything/oauth2-types
+```
+
+## Usage
+```ts
+include * from '@oauth-everything/oauth2-types';
+```
---
+++ src/TypeUtil.ts
@@ -0,0 +1,53 @@
+import { StrategyOptions, StrategyOptionsWithRequest } from "passport-oauth2";
+
+type Optionalize<T, U extends keyof T> = Omit<T, U> & Partial<Pick<T, U>>;
+
+export type ExtendableStrategyOptions<TMixin> = Optionalize<
+    StrategyOptions,
+    'authorizationURL' | 'tokenURL'
+> & TMixin;
+
+export type ExtendableStrategyOptionsWithRequest<TMixin> = Optionalize<
+    StrategyOptionsWithRequest,
+    'authorizationURL' | 'tokenURL'
+> & TMixin;
+
+export type OAuth2VerifyCallback<TUser = object, TInfo = object> = (
+    err?: Error | null,
+    user?: TUser,
+    info?: TInfo
+) => void;
+
+export type OAuth2VerifyFunction<TProfile, TUser, TInfo> = (
+    accessToken: string,
+    refreshToken: string,
+    profile: TProfile,
+    verified: OAuth2VerifyCallback<TUser, TInfo>
+) => void;
+
+
+export type OAuth2VerifyFunctionWithResults<TResults, TProfile, TUser, TInfo> = (
+    accessToken: string,
+    refreshToken: string,
+    results: TResults,
+    profile: TProfile,
+    verified: OAuth2VerifyCallback<TUser, TInfo>
+) => void;
+
+
+export type OAuth2VerifyFunctionWithRequest<TProfile, TUser, TInfo> = (request: Request,
+    accessToken: string,
+    refreshToken: string,
+    profile: TProfile,
+    verified: OAuth2VerifyCallback<TUser, TInfo>
+) => void;
+
+
+export type OAuth2VerifyFunctionWithRequestAndResults<TResults, TProfile, TUser, TInfo> = (
+    request: Request,
+    accessToken: string,
+    refreshToken: string,
+    results: TResults,
+    profile: TProfile,
+    verified: OAuth2VerifyCallback<TUser, TInfo>
+) => void;
